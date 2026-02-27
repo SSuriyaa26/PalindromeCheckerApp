cab40aab3ef8003c4a1c65c8ddadd613212fabce
SSuriyaa26 <ssuriyaa26@gmail.com>
2026-02-27 14:51:24 +0530

    UC3

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,25 +1,24 @@
 import java.util.Scanner;
 public class PalindromeCheckerApp {
-   public  void main(String[] args) {
+   public static void main(String[] args) {
        System.out.println("Welcome to the palindrome Checker Management System \nVersion : 1.0\nSystem initialized sucessfully.");
        Scanner input = new Scanner(System.in);
        System.out.print("Input text: ");
        String text=input.nextLine();
        boolean flag=true;
-       for (int i=0;i<=text.length()/2;i++){
-           if(text.charAt(i)==text.charAt(text.length()-i-1)){
-               continue;
-
-           }
-           else{
-               flag=false;
-               break;
-           }
+       String comptext="";
+       for(int i =text.length()-1;i>=0;i--){
+           comptext += text.charAt(i);
        }
-
-       System.out.println("Is it a Palindrome? : "+flag);
-
-
+        for(int i=0;i<text.length();i++){
+            if (comptext.charAt(i)==text.charAt(i)){
+                continue;
+            }
+            else{
+                flag = false;
+            }
+        }
+        System.out.println("Is it a  palindrome? : "+flag);
    }
 
 }
