a3728fbcaf1c12bcbe858d6436c421e411477bc4
SSuriyaa26 <ssuriyaa26@gmail.com>
2026-02-27 14:04:49 +0530

    Use case 1

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,5 @@
+public class UseCase1PalindromeCheckerApp {
+    void main(String[] args) {
+        System.out.println("Welcome to the palindrome Checker Management System \nVersion : 1.0\nSystem initialized sucessfully.");
+    }
+}
