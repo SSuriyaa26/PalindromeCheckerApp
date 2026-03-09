8bc04ba08a64dec89766c50281b1f590a146d0f5
SSuriyaa26 <ssuriyaa26@gmail.com>
2026-03-09 14:41:33 +0530

    UC9: Recursive Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,22 +1,23 @@
 import java.util.*;
 
 public class PalindromeCheckerApp {
+    private static boolean check(String s, int start,int end){
+        if(start>=end){
+            return true;
+        }
+        if(s.charAt(start)==s.charAt(end)){
+            return check (s,start+1,end-1);
+        }
+        else{
+            return false;
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to the palindrome Checker Management System \nVersion : 1.0\nSystem initialized sucessfully.");
         Scanner input = new Scanner(System.in);
         System.out.print("Input text: ");
         String text = input.nextLine();
-        boolean flag = true;
-        LinkedList<Character> list =new LinkedList<>();
-        for(char c: text.toCharArray()){
-            list.add(c);
-        }
-        while(list.size()>1){
-            if(list.removeFirst()!=list.removeLast()){
-                flag = false;
-                break;
-            }
-        }
+        boolean flag = check(text,0,text.length()-1);
         System.out.println("Is it a palindrome? : "+flag);
         input.close();
     }
